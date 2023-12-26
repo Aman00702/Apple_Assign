148a79a8513a4d50bedccf10ae9b28bcfd931154
Aman Agarwal <87354054+Aman00702@users.noreply.github.com>
2023-12-26 19:02:27 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { Navbar } from "./Header/Navbar";
+import HomeColContainer from "./container/HomeColContainer";
+import HomeContainer from "./container/HomeContainer";
+import SearchInput from "./container/SearchInput";
+import Product from "./Product/Product";
+import ProductCard from "./Product/ProductCard";
+import Sidebar from "../pages/Sidebar";
+
+export {
+   Navbar,
+   HomeColContainer,
+   HomeContainer,
+   SearchInput,
+   Product,
+   ProductCard,
+   Sidebar,
+}
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import store from "./utils/store.js";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import { Product } from "./components/index.js";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
